79a3760caadfd0b0c7edc888761f0837507b88c4
nogodfyb <120626481@qq.com>
2020-06-30 13:58:25 +0800

    build dist

--- shimo-ui/src/main-prod.js
+++ shimo-ui/src/main-prod.js
@@ -5,7 +5,7 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
-axios.defaults.baseURL = 'http://112.74.51.188:8080/production/'
+axios.defaults.baseURL = 'http://192.168.13.65:8081/shimo/'
 // 允许携带cookie
 axios.defaults.withCredentials = true
 Vue.prototype.$http = axios
